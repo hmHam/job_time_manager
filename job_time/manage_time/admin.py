0a4b7cd343513a76bbe657f36970f0dd8753ac13
ponchTaro <ponchTaro@gmail.com>
2020-01-12 02:26:37 +0900

    admin site

--- job_time/manage_time/admin.py
+++ job_time/manage_time/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from job_time.manage_time.models import (
+    Member,
+    LineID,
+    Attendance,
+    Break
+)
 # Register your models here.
+admin.site.register(Member)
+admin.site.register(LineID)
+admin.site.register(Attendance)
+admin.site.register(Break)
