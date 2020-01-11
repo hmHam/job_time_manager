46159f5414538c549547445a0a85585ecc5912d2
ponchTaro <ponchTaro@gmail.com>
2020-01-11 21:57:00 +0900

    allow hosts

--- job_time/settings.py
+++ job_time/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hh--s567c^jz)76fxgbi6@u%@&@wrnvdp_!8)43emrtbk@$b)5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['job-time.herokuapp.com']
 
 
 # Application definition
