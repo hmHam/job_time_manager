bd62f6148cd9a2e81847209d3ded014ae1d42755
shigekato <31150623+shigekato@users.noreply.github.com>
2021-11-04 18:53:44 +0900

    Update settings.py

--- job_time/settings.py
+++ job_time/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hh--s567c^jz)76fxgbi6@u%@&@wrnvdp_!8)43emrtbk@$b)5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
